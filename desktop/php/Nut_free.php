891b5f1ba3cb66dbfff77232fbac5f98dc63a3ee
megastorage <stephane.flavigny@gmail.com>
2019-04-06 23:21:24 +0200

    Update Nut_free.php

--- desktop/php/Nut_free.php
+++ desktop/php/Nut_free.php
@@ -43,7 +43,7 @@ $eqLogics = eqLogic::byType($plugin->getId());
 				}
 					echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;' . $opacity . '" >';
 					echo "<center>";
-					echo '<img src="plugins/Nut_free/doc/images/nut_free_icon.png" height="105" width="95" />';
+					echo '<img src="plugins/Nut_free/plugin_info/Nut_free_icon.png" height="105" width="95" />';
 					echo "</center>";
 					echo '<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;"><center>' . $eqLogic->getHumanName(true, true) . '</center></span>';
 					echo '</div>';
