429f13a06aaef0e51db168d80c317fd7e766a065
megastorage <stephane.flavigny@gmail.com>
2020-11-15 10:51:22 +0100

    Update Nut_free.class.php

--- core/class/Nut_free.class.php
+++ core/class/Nut_free.class.php
@@ -372,20 +372,20 @@ class Nut_free extends eqLogic {
 			$pass = $this->getConfiguration('password');
 			$port = $this->getConfiguration('portssh');
 			log::add('Nut_free', 'debug',' -----------------------------------------------------' );
-			log::add('Nut_free', 'debug',' 			Connexion SSH' );
+			log::add('Nut_free', 'debug','			Connexion SSH' );
 			if (!$sshconnection = ssh2_connect($ip,$port)){
-				log::add('Nut_free', 'error', 'connexion SSH KO pour ' . $equipement );
+				log::add('Nut_free', 'error', '			connexion SSH KO pour ' . $equipement );
 				log::add('Nut_free', 'debug', 'connexion SSH KO pour ' . $equipement );
 				$cnx_ssh = 'KO';
 			}else{
-				log::add('Nut_free', 'debug', 'Liaison ok: ' . $equipement );
+				log::add('Nut_free', 'debug', '			Liaison ok: ' . $equipement );
 				if (!ssh2_auth_password($sshconnection,$user,$pass)){
 				log::add('Nut_free', 'error', 'Authentification SSH KO pour ' . $equipement );
 				log::add('Nut_free', 'debug', 'Authentification SSH KO pour ' . $equipement );
 				$cnx_ssh = 'KO';
 			
 				}else{
-					log::add('Nut_free', 'debug', 'Authentification SSH OK pour ' . $equipement );
+					log::add('Nut_free', 'debug', '			Authentification SSH OK pour ' . $equipement );
 					$upscmd = "upsc -l 2>&1 | grep -v '^Init SSL'";
 					$ups_output = ssh2_exec($sshconnection, $upscmd); 
 					stream_set_blocking($ups_output, true);
